5df6744df6a1ac55de6281c10fd7036e17c2a6b6
Tuan Vu Minh <tuan38@seznam.cz>
2023-07-30 18:43:41 +0200

    Rozpracovaný projekt

--- src/com/Order.java
+++ src/com/Order.java
@@ -136,18 +136,4 @@ public class Order {
     public String toString() {
         return "Objednávka č. " + numberOfOrder + " " + dish + " čas objednávky " + orderedTime;
     }
-
-    public String exportOrder() {
-        return "Order{" +
-                "numberOfOrder=" + numberOfOrder +
-                ", table=" + table +
-                ", waiter=" + waiter +
-                ", dish=" + dish +
-                ", numberOfOrderedDishes=" + numberOfOrderedDishes +
-                ", orderedTime=" + orderedTime +
-                ", notesOfCustomer='" + notesOfCustomer + '\'' +
-                ", fulfilmentTime=" + fulfilmentTime +
-                ", totalPriceOfOrder=" + totalPriceOfOrder +
-                '}';
-    }
 }
