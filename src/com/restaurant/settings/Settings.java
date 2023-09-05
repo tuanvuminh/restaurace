18801a1acdab978c3752418e850ecd6b48e10a31
„Tuan <tuan38@seznam.czgit config --global user.name Tuan>
2023-09-05 15:12:08 +0200

    Reorganizace

--- src/com/restaurant/settings/Settings.java
+++ src/com/restaurant/settings/Settings.java
@@ -1,37 +1,34 @@
 package com.restaurant.settings;
 
-import java.io.File;
-
 public class Settings {
 
-    private static final String dataDirectory = "data";
-
     public static String filenameTest() {
-        return dataDirectory + File.separator + "test.txt";
+        return "data/test.txt";
     }
 
     public static String filenameOrders() {
-        return dataDirectory + File.separator + "orders.txt";
+        return "data/orders.txt";
     }
 
     public static String filenameMenu() {
-        return dataDirectory + File.separator + "menu.txt";
+        return "data/menu.txt";
     }
 
     public static String filenameCookbook() {
-        return dataDirectory + File.separator + "cookbook.txt";
+        return "data/cookbook.txt";
     }
 
     public static String filenameNewCookbook() {
-        return dataDirectory + File.separator + "new-cookbook.txt";
+        return "data/new-cookbook.txt";
     }
 
     public static String filenameNewMenu() {
-        return dataDirectory + File.separator + "new-menu.txt";
+        return "data/new-menu.txt";
     }
 
-    public static String delimiter() {
+    public static String delimiter () {
         return "\t";
     }
 }
+
 
